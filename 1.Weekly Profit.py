6a8b280ae0ce4a9c0c515b96dfab5e3e98b61e8c
Kursad <kursad.demirdogen@gmail.com>
2020-12-05 07:12:25 +0300

    weekly profit

---
+++ 1.Weekly Profit.py
@@ -0,0 +1,34 @@
+PROBLEM:
+Problem : If you had deposited a coin on the cryptocurrency exchange that brought 7% fixed profit daily for a week, how much would your $ 1000 reach at the end of the 7th day?
+
+SOLUTİON:
+
+money = 1000
+
+money = money + money*0.07
+
+print("1.gün sonu:" , money)
+
+money = money + money*0.07
+
+print("2.gün sonu:" , money)
+
+money = money + money*0.07
+
+print("3.gün sonu:" , money)
+
+money = money + money*0.07
+
+print("4.gün sonu:" , money)
+
+money = money + money*0.07
+
+print("5.gün sonu:" , money)
+
+money = money + money*0.07
+
+print("6.gün sonu:" , money)
+
+money = money + money*0.07
+
+print("7.gün sonu:" , money)
