7d646246c48821c7684957b8069ea98ee90c4489
KursadDD <kursad.demirdogen@gmail.com>
2020-12-22 12:04:33 +0300

    doubler

---
+++ 18.Doubler.py
@@ -0,0 +1,9 @@
+PROBLEM:
+Given two integer values, return their sum. If the two values are the same, then return double their sum.
+
+SOLUTİON:
+def sum_double(x, y):
+    if x == y:
+        return (x+y)*2
+    else:
+        return (x+y)
